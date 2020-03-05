5e46f2cdf6e7f50301408cf2c452288e40060fd4
abeltie <43939062+abeltie@users.noreply.github.com>
2020-03-05 22:19:03 +0800

    Update Wuhan Virus Outbreak Distribution In China.py

--- Wuhan Virus Outbreak Distribution In China.py
+++ Wuhan Virus Outbreak Distribution In China.py
@@ -14,28 +14,6 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import jsonpath
 
-
-url = 'https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5&&callback=&_=%d'
-data = json.loads(requests.get(url=url).json()['data'])
-# pron_num = [i for i in range(len(data['areaTree'][0]['children']))]
-# city_num = [j for j in range(len(data['areaTree'][0]['children'][0]['children']))]
-
-"""
-for m in range(len(data['areaTree'][0]['children'])):
-	for n in range(len(data['areaTree'][0]['children'][m]['children'])):
-		info = {
-			'country': data['areaTree'][0]['name'],
-			'province': data['areaTree'][0]['children'][m]['name'],
-			'city': data['areaTree'][0]['children'][m]['children'][n]['name'],
-			'total_confirm': data['areaTree'][0]['children'][m]['children'][n]['total']['confirm'],
-			'total_suspect': data['areaTree'][0]['children'][m]['children'][n]['total']['suspect'],
-			'total_dead': data['areaTree'][0]['children'][m]['children'][n]['total']['dead'],
-			'total_heal': data['areaTree'][0]['children'][m]['children'][n]['total']['heal'],
-			'today_confirm': data['areaTree'][0]['children'][m]['children'][n]['today']['confirm']
-		}
-		lis.append(info)
-"""
-
 plt.rcParams['font.sans-serif'] = ['FangSong']
 plt.rcParams['axes.unicode_minus'] = False
 
@@ -67,10 +45,6 @@ def catch_distribution():
 	list_province = jsonpath.jsonpath(china_data, expr='$[*].name')
 	list_province_confirm = jsonpath.jsonpath(china_data, expr='$[*].total.confirm')
 	dic_province_confirm = dict(zip(list_province, list_province_confirm))
-	"""for item in data_dis:
-		if item['province'] not in data_dis:
-			data_dis.update({item['province']: 0})
-			data_dis[item['province']] += int(item['total_confirm'])"""
 	return dic_province_confirm
 
 
@@ -79,18 +53,18 @@ print(area_data)
 
 
 def plot_daily():
-	date_list, confirm_list, suspect_list, dead_list, heal_list = catch_daily()  # 获取数据
+	date_list, confirm_list, suspect_list, dead_list, heal_list = catch_daily()  # obtain data
 	plt.figure('2019-nCoV Outbreak Statistics', facecolor='#f4f4f4', figsize=(10, 8))
 	plt.title('2019-nCoV Outbreak Curve', fontsize=20)
 	plt.plot(date_list, confirm_list, label='Diagnosed')
 	plt.plot(date_list, suspect_list, label='Suspected')
 	plt.plot(date_list, dead_list, label='Deaths')
 	plt.plot(date_list, heal_list, label='Cured')
-	plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))  # 格式化时间轴标注
-	plt.gcf().autofmt_xdate()  # 优化标注（自动倾斜）
-	plt.grid(linestyle=':')  # 显示网
-	plt.legend(loc='best')  # 显示图例
-	plt.savefig('2019-nCoV Outbreak Curve.png')  # 保存为文件
+	plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))  # data format
+	plt.gcf().autofmt_xdate()  # get current figure and nicely format the x-axis
+	plt.grid(linestyle=':')  # show grid
+	plt.legend(loc='best')  # show legend
+	plt.savefig('2019-nCoV Outbreak Curve.png')
 	plt.show()
 
 
@@ -111,11 +85,11 @@ def plot_distribution():
 	]
 	labels = ['1-9 ppl', '10-99 ppl', '100-999 ppl', '>1000 ppl']
 	fig = matplotlib.figure.Figure()
-	fig.set_size_inches(10, 8)  # 设置绘图板尺寸
+	fig.set_size_inches(10, 8)
 	axes = fig.add_axes((0.1, 0.12, 0.8, 0.8))  # rect = l,b,w,h
 	m = Basemap(llcrnrlon=lon_min, urcrnrlon=lon_max, llcrnrlat=lat_min, urcrnrlat=lat_max, resolution='l', ax=axes)
-	# m = Basemap(projection='ortho', lat_0=30, lon_0=105, resolution='l', ax=axes)
-	# m = Basemap(projection='ortho', lat_0=30, lon_0=105, resolution='l', ax=axes)
+	# m = Basemap(projection='ortho', lat_0=30, lon_0=105, resolution='l', ax=axes) #show bigger region
+	# m = Basemap(projection='ortho', lat_0=30, lon_0=105, resolution='l', ax=axes) #show bigger region
 	m.readshapefile(
 		'C:/Users/Abel Tie/PycharmProjects/Wuhan Virus Outbreak/venv/Lib/site-packages/china-shapefiles/china',
 		'province', drawbounds=True)
@@ -123,10 +97,10 @@ def plot_distribution():
 		'C:/Users/Abel Tie/PycharmProjects/Wuhan Virus '
 		'Outbreak/venv/Lib/site-packages/china-shapefiles/china_nine_dotted_line',
 		'section', drawbounds=True)
-	m.drawcoastlines(color='black')  # 洲际线
-	m.drawcountries(color='black')  # 国界线
-	m.drawparallels(np.arange(lat_min, lat_max, 10), labels=[1, 0, 0, 0])  # 画经度线
-	m.drawmeridians(np.arange(lon_min, lon_max, 10), labels=[0, 0, 0, 1])  # 画纬度线
+	m.drawcoastlines(color='black')
+	m.drawcountries(color='black')
+	m.drawparallels(np.arange(lat_min, lat_max, 10), labels=[1, 0, 0, 0])
+	m.drawmeridians(np.arange(lon_min, lon_max, 10), labels=[0, 0, 0, 1])
 
 	global count_iter
 	count_iter = 0
@@ -134,26 +108,26 @@ def plot_distribution():
 	for info, shape in zip(m.province_info, m.province):
 		pname = info['OWNER'].strip('\x00')
 		fcname = info['FCNAME'].strip('\x00')
-		if pname != fcname:  # 不绘制海岛
+		if pname != fcname:  # doesn't map
 			continue
 		is_reported = False
-		for key in data.keys():
+		for key in area_data.keys():
 			count_iter += 1
 			if key in pname:
 				is_reported = True
-				if data[key] == 0:
+				if area_data[key] == 0:
 					color = '#f0f0f0'
 					poly = Polygon(shape, facecolor=color, edgecolor=color)
 					axes.add_patch(poly)
-				elif data[key] < 10:
+				elif area_data[key] < 10:
 					color = '#ffaa85'
 					poly = Polygon(shape, facecolor=color, edgecolor=color)
 					axes.add_patch(poly)
-				elif data[key] < 100:
+				elif area_data[key] < 100:
 					color = '#ff7b69'
 					poly = Polygon(shape, facecolor=color, edgecolor=color)
 					axes.add_patch(poly)
-				elif data[key] < 1000:
+				elif area_data[key] < 1000:
 					color = '#bf2121'
 					poly = Polygon(shape, facecolor=color, edgecolor=color)
 					axes.add_patch(poly)
